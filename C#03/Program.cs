ab95fa3f1ffdcddcf46d3478af1d9089e775ee9a
Moho32bit00 <mohamed.man2007@gmail.com>
2026-09-04 17:38:57 +0300

    3rd answer

--- C#03/Program.cs
+++ C#03/Program.cs
@@ -23,6 +23,12 @@
             //};
             //Console.WriteLine(sol);
             #endregion
+
+            #region q3 
+            int pages = 464;
+            string sol = (pages > 300) ? "Long Book" : "Short Book";
+            Console.WriteLine(sol);
+            #endregion
         }
     }
 }
